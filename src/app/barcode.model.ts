ea54d1116ea31811ab3309423c719bd4044808ee
mrunivil <mr.univil@freenet.de>
2020-06-09 10:12:22 +0200

    working bar code generation

--- src/app/barcode.model.ts
+++ src/app/barcode.model.ts
@@ -1,28 +1,45 @@
 export class BarcodeModel {
   digits: number[];
   constructor() {
-    this.digits = [0,5,1,0,0,0,0,1,2,5,1,7];
-    // for (let i = 0; i < 12; i++) {
-    //   this.digits.push(Math.floor(Math.random() * 10));
-    // }
+    this.digits = [];
+    this.digits.push(0);
+    for (let i = 1; i < 11; i++) {
+    this.digits.push(Math.floor(Math.random() * 10));
+    }
+    const test =
+      3 *
+        (this.digits[0] +
+          this.digits[2] +
+          this.digits[4] +
+          this.digits[6] +
+          this.digits[8] +
+          this.digits[10]) +
+      (this.digits[1] +
+        this.digits[3] +
+        this.digits[5] +
+        this.digits[7] +
+        this.digits[9]);
+    let dec = (parseInt((test / 10).toFixed(1) + "".split(".")[0]) + 1) * 10;
+    this.digits.push(dec-test)
   }
   toString() {
     return this.digits.join(",");
   }
   toBinary() {
     let ret = [1, 0, 1];
-    for (let i = 0; i < this.digits.length/2; i++) {
+    for (let i = 0; i < this.digits.length / 2; i++) {
       ret = ret.concat(this.digitToBinary(this.digits[i]));
     }
     ret = ret.concat([0, 1, 0, 1, 0]);
-    for (let i = this.digits.length/2; i <this.digits.length; i++) {
-      ret = ret.concat(this.digitToBinary(this.digits[i]).map(b=>Math.abs(b-1)));
+    for (let i = this.digits.length / 2; i < this.digits.length; i++) {
+      ret = ret.concat(
+        this.digitToBinary(this.digits[i]).map(b => Math.abs(b - 1))
+      );
     }
-    ret = ret.concat([1,0,1]);
-    console.dir(ret)
+    ret = ret.concat([1, 0, 1]);
+    console.dir(ret);
     return ret;
   }
-
   digitToBinary(digit: number) {
     switch (digit) {
       case 0:
